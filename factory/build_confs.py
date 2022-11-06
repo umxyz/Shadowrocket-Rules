4c4269f8f5292ea522554cbc01ff6621a318c9c2
umxyz <229781766@qq.com>
2022-11-06 18:03:08 +0800

    Nightly build
    已合并最新的去广告规则及 GFWList

--- factory/build_confs.py
+++ factory/build_confs.py
@@ -58,7 +58,7 @@ values['build_time'] = time.strftime("%Y-%m-%d %H:%M:%S")
 values['top500_proxy']  = getRulesStringFromFile('resultant/top500_proxy.list', 'Proxy')
 values['top500_direct'] = getRulesStringFromFile('resultant/top500_direct.list', 'Direct')
 
-values['ad'] = getRulesStringFromFile('resultant/ad.list', 'Reject')
+# values['ad'] = getRulesStringFromFile('resultant/ad.list', 'Reject')
 
 values['manual_direct'] = getRulesStringFromFile('manual_direct.txt', 'Direct')
 values['manual_proxy']  = getRulesStringFromFile('manual_proxy.txt', 'Proxy')
